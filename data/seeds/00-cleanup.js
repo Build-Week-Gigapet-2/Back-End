8693da18636a21af0ab4c4a9ad55b0530199596f
Barbara Mayfield <bmayfield1991@gmail.com>
2020-02-03 20:21:33 -0600

    Added date string to food_items table and seed data

--- data/seeds/00-cleanup.js
+++ data/seeds/00-cleanup.js
@@ -1,7 +1,7 @@
 exports.seed = async function(knex) {
   await knex("children_food_item").truncate()
-  await knex("food_items").del()
-  await knex("food_category").truncate()
+  await knex("food_items").delete()
+  await knex("food_category").delete()
   await knex("children").truncate()
   await knex("users").truncate()
 }
--- data/seeds/04-food-items.js
+++ data/seeds/04-food-items.js
@@ -1,7 +1,7 @@
 exports.seed = async (knex) => {
   await knex("food_items").insert([
-    { id: 1, category_id: 4, name: "Chicken" },
-    { id: 2, category_id: 7, name: "Marmite" },
-    { id: 3, category_id: 2, name: "Tomato Soup" }
+    { id: 1, category_id: 4, name: "Chicken", date: "02-03-2020" },
+    { id: 2, category_id: 7, name: "Marmite", date: "01-16-2020" },
+    { id: 3, category_id: 2, name: "Tomato Soup", date: "01-01-2020" }
   ])
 }
