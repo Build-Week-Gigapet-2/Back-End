2a42292949536b61f90c1ea76894d4804f1d2c22
Barbara Mayfield <bmayfield1991@gmail.com>
2020-02-04 19:52:47 -0600

    Removed interpolated string in successful user register

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -17,7 +17,7 @@ router.post('/register', async (req, res, next) => {
         } else {
         const newUser = await userModel.addUser({ username, password })
 
-        res.status(200).json({ message: `User ${user.username} successfully registered!`}, newUser)
+        res.status(200).json({ message: `User successfully registered!`}, newUser)
         }
     } catch(err) {
         res.status(500).json({ message: "Unable to register user" })
